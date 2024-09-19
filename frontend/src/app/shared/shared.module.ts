6c7de632be0d0eebd3d2a3bfda6f0cd13dfeec62
Guido <gvm0898@gmail.com>
2024-09-20 00:55:34 +0200

    Categories are loading in home

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,19 +1,31 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 
+import { ListCategoriesComponent } from './list-categories/list-categories.component';
+import { CardCategoryComponent } from '../shared/card-category/card-category.component';
+
+
 @NgModule({
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   declarations: [
     HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    CommonModule
+    FooterComponent,
+    ListCategoriesComponent,
+    CardCategoryComponent
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ListCategoriesComponent,
+    CardCategoryComponent
   ]
 })
 
