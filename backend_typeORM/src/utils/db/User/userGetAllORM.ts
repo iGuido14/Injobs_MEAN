0d93d80f894ddba2ee4aa991ae520ccc238665f5
Guido <gvm0898@gmail.com>
2024-10-28 17:23:16 +0100

    deleted backend_prisma completelly to remake it from scratch

--- backend_typeORM/src/utils/db/User/userGetAllORM.ts
+++
@@ -1,7 +0,0 @@
-import { getRepository } from "typeorm";
-import { User } from "../../../entities/User";
-
-export default async function getAllUsersORM() {
-    const users = await getRepository(User).find();
-    return users;
-}
