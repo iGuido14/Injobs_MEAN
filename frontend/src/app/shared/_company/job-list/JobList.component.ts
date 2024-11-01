af39815d22ac86131ef05050fd95e01f0ef3f86a
Guido <gvm0898@gmail.com>
2024-11-01 22:30:57 +0100

    job list component for company

---
+++ frontend/src/app/shared/_company/job-list/JobList.component.ts
@@ -0,0 +1,35 @@
+import { RouterLink, RouterModule } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product, ProductService, UserService } from 'src/app/core';
+
+@Component({
+  selector: 'app-company-job-list',
+  templateUrl: './jobList.component.html',
+  styleUrls: ['./jobList.component.css'],
+  standalone: true,
+  imports: [
+    RouterModule,
+    RouterLink,
+    CommonModule,
+  ]
+})
+
+export class jobListComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(
+    private productService: ProductService,
+    private userService: UserService
+  ) { }
+
+  ngOnInit(): void {
+    const user = this.userService.getCurrentUser();
+
+    this.productService.get_products_company(user.username).subscribe((data: Product[]) => {
+      console.log(data);
+      this.products = data;
+    })
+  }
+
+}
